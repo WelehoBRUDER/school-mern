c7cc0e3cd43bf8c3063942afddff1a36cb4bef84
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2020-11-11 14:55:05 +0200

    basic ejs shit

--- backend/controllers/admin.js
+++ backend/controllers/admin.js
@@ -17,19 +17,77 @@ exports.getIndex = async (req, res) => {
 
 exports.getGame = async (req, res) => {
     const gameId = req.params.gameId;
-    console.log(gameId);
 
     if (gameId.match(/^[0-9a-fA-F]{24}$/)) {
         // Yes, it's a valid ObjectId, proceed with `findById` call.
 
         const game = await Game.findById(gameId, (game) => game);
         try {
-            console.log(game);
+            console.log(game.name + " rendered!");
             res.status(200).render('game', { game: game });
         } catch (error) {
             console.log(error);
         }
     }
+};
+
+exports.postDelete = async (req, res) => {
+    const gameId = req.body.gameId;
+
+    const game = await Game.findByIdAndRemove(gameId, (data) => data);
+
+    try {
+        console.log("Item deleted!");
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.getEditGame = async (req, res) => {
+    const gameId = req.params.gameId;
+    console.log(gameId);
+
+    const editMode = req.query.edit;
+
+    if (!editMode) {
+        return res.redirect('/');
+    }
+
+    const game = await Game.findById(gameId);
+
+    try {
+        if (!gameId) {
+            return res.redirect('/');
+        }
+        console.log(game);
+        res.status(200).render('edit-game', { game: game, editing: editMode });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+exports.postEditGame = (req, res) => {
+    const gameId = req.body.gameId;
+    const { name, image, description, difficulty, length } = req.body;
+
+    Game.findById(gameId)
+        .then((game) => {
+            game.name = name;
+            game.image = image;
+            game.description = description;
+            game.difficulty = difficulty;
+            game.length = length;
+
+            return game.save();
+        })
+        .then(() => {
+            console.log('Item Updated');
+            res.status(201).redirect('/');
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 };
 
 exports.getAddGame = (req, res) => {
--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -9,6 +9,12 @@ router.get('/add-game', adminController.getAddGame);
 
 router.post('/add-game', adminController.postGame);
 
+router.post('/edit-game', adminController.postEditGame);
+
+router.get('/edit-game/:gameId', adminController.getEditGame);
+
+router.post('/delete', adminController.postDelete);
+
 router.get('/:gameId', adminController.getGame);
 
 module.exports = router;
